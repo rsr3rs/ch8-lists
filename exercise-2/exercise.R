5c7875b723fe78cb9e695370494e7d96dc8d09d1
Robin-Yang <yangy87@uw.edu>
2018-04-05 21:59:56 -0700

    finish exercises

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,41 +2,47 @@
 
 # Create a *list* of 10 random numbers. Use the `runif()` function to make a 
 # vector of random numbers, then use `as.list()` to convert that to a list
-
+my_list <- as.list(runif(10, 0, 10))
 
 # Use `lapply()` to apply the `round()` function to each number, rounding it to 
 # the nearest 0.1 (one decimal place)
-
+lapply(my_list, round, 0.1)
 
 # Create a variable 'sentence' that contains a sentence of text (something 
 # longish). Make the sentence lowercase; you can use a function to help.
-
+sentence <- tolower("Lists are extremely useful for organizing data. They allow you
+                    to group together data like a person¡¯s name (characters), job title
+                    (characters), salary (number), and whether they are in a union 
+                    (logical)¡ªand you don¡¯t have to remember whether whether the 
+                    person¡¯s name or title was the first element!")
 
 # Use the `strsplit()` function to split the sentence into a vector of letters.
 # Hint: split on `""` to split every character
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
-
+split_sentence <- strsplit(sentence, "")
 
 # Extract the vector of letters from the resulting list
-
+letters <- split_sentence[[1]]
 
 # Use the `unique()` function to get a vector of unique letters
-
+unique_letters <- unique(letters)
 
 # Define a function `count_occurrences` that takes in two parameters: a letter 
 # and a vector of letters. The function should return how many times that letter
 # occurs in the provided vector.
 # Hint: use a filter operation!
-
+count_occurrences <- function(l, v) {
+  return(length(v[v == l]))
+}
 
 # Call your `count_occurrences()` function to see how many times the letter 'e'
 # is in your sentence.
-
+count_occurrences("e", letters)
 
 # Use `sapply()` to apply your `count_occurrences()` function to each unique 
 # letter in the vector to determine their frequencies.
 # Convert the result into a list (using `as.list()`).
-
+result <- as.list(sapply(unique_letters, count_occurrences, v = letters))
 
 # Print the resulting list of frequencies
-
+print(result)
